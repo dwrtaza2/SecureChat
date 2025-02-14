4fda74a9e3914700ff35a3b9aafd6bf4668cfe37
dwrtaza2 <111820841+dwrtaza2@users.noreply.github.com>
2025-02-14 13:43:22 -0800

    fixed dependecy problems

--- mongodb_connect.js
+++ mongodb_connect.js
@@ -1,11 +1,9 @@
-equire('dotenv').config();
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
             tls: true,
             tlsAllowInvalidCertificates: true
         });
